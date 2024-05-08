fe8586eee4cb3a6521f2b4c83f3a5ff8ed6480b6
Sylvain CALINON <sylvain.calinon@idiap.ch>
2024-05-08 23:11:46 +0200

    codes added

---
+++ runserver.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# SPDX-FileCopyrightText: 2023 Idiap Research Institute <contact@idiap.ch>
+#
+# SPDX-FileContributor: Philip Abbet <philip.abbet@idiap.ch>
+#
+# SPDX-License-Identifier: MIT
+
+python3 -m http.server

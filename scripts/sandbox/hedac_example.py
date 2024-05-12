7d6d1912dd29bed1c95e03ce4d50137268fcd8af
Sylvain CALINON <sylvain.calinon@idiap.ch>
2024-05-13 06:34:06 +0900

    HEDAC changed with Cem suggestions

--- scripts/sandbox/hedac_example.py
+++ scripts/sandbox/hedac_example.py
@@ -3,8 +3,11 @@ param.x0 = np.array([
     [.2, .3],
 ])
 
-# Number of gaussians
+# Number of Gaussians
 param.nbGaussian = 2
+
+# Timesteps for integrating diffusion (higher values lead to more global exploration, [1,100])
+param.nb_diffusion_timesteps = 100  
 
 
 def controlCommand(agents, goal_density, coverage_density, heat, coverage_block, param):
@@ -39,17 +42,16 @@ def controlCommand(agents, goal_density, coverage_density, heat, coverage_block,
     source = np.maximum(diff, 0) ** 2
     source = normalize_mat(source) * param.area
 
-    current_heat = np.zeros((param.height, param.width))
-
     # 2-D heat equation (Partial Differential Equation)
     # In 2-D we perform this second-order central for x and y.
     # Note that, delta_x = delta_y = h since we have a uniform grid.
     # Accordingly we have -4.0 of the center element.
-
+    
     # At boundary we have Neumann boundary conditions which assumes
     # that the derivative is zero at the boundary. This is equivalent
     # to having a zero flux boundary condition or perfect insulation.
-    current_heat[1:-1, 1:-1] = param.dt * (
+    for i in range(param.nb_diffusion_timesteps):
+      heat[1:-1, 1:-1] = param.dt * (
         (
             + param.alpha[0] * offset(heat, 1, 0)
             + param.alpha[0] * offset(heat, -1, 0)
@@ -60,9 +62,7 @@ def controlCommand(agents, goal_density, coverage_density, heat, coverage_block,
         / (param.dx * param.dx)
         + param.source_strength * offset(source, 0, 0)
     ) + offset(heat, 0, 0)
-
-    heat = current_heat.astype(np.float32)
-
+    
     # Calculate the first derivatives mind the order x and y
     gradient_y, gradient_x = np.gradient(heat, 1, 1)
 
--- scripts/sandbox3d/hedac_example.py
+++ scripts/sandbox3d/hedac_example.py
@@ -1,8 +1,11 @@
 # Initial robot state
 param.x0 = [0.5, -0.3, 0.0, -1.8, 0.0, 1.5, 1.0]
 
-# Number of gaussians
+# Number of Gaussians
 param.nbGaussian = 2
+
+# Timesteps for integrating diffusion (higher values lead to more global exploration, [1,25])
+param.nb_diffusion_timesteps = 25 
 
 
 def ergodicControl(x, agent, goal_density, coverage_density, heat, coverage_block, param):
@@ -38,17 +41,16 @@ def ergodicControl(x, agent, goal_density, coverage_density, heat, coverage_bloc
     source = np.maximum(diff, 0) ** 3
     source = normalize_mat(source) * param.area
 
-    current_heat = np.zeros((param.depth, param.height, param.width))
-
-    # 2-D heat equation (Partial Differential Equation)
-    # In 2-D we perform this second-order central for x and y.
-    # Note that, delta_x = delta_y = h since we have a uniform grid.
-    # Accordingly we have -4.0 of the center element.
-
+    # 3-D heat equation (Partial Differential Equation)
+    # In 3-D we perform this second-order central for x,y and z
+    # Note that, delta_x = delta_y = delta_z = h since we have a uniform grid.
+    # Accordingly we have -6.0 of the center element.
+    
     # At boundary we have Neumann boundary conditions which assumes
     # that the derivative is zero at the boundary. This is equivalent
     # to having a zero flux boundary condition or perfect insulation.
-    current_heat[1:-1, 1:-1, 1:-1] = param.dt * (
+    for i in range(param.nb_diffusion_timesteps):
+      heat[1:-1, 1:-1, 1:-1] = param.dt * (
         (
             + param.alpha[2] * offset(heat, 1, 0, 0)
             + param.alpha[2] * offset(heat, -1, 0, 0)
@@ -61,9 +63,7 @@ def ergodicControl(x, agent, goal_density, coverage_density, heat, coverage_bloc
         / (param.dx * param.dx * param.dx)
         + param.source_strength * offset(source, 0, 0, 0)
     ) + offset(heat, 0, 0, 0)
-
-    heat = current_heat.astype(np.float32)
-
+    
     # Calculate the first derivatives (mind the order x, y and z)
     gradient_z, gradient_y, gradient_x = np.gradient(heat, 1, 1, 1)
 
